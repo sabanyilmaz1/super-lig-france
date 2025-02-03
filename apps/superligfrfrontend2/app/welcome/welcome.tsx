5781abcb0438029fe71552c63a270549551df6c1
Saban yilmaz <syilmaz002@ensc.fr>
2025-02-03 21:44:53 +0100

    Add postcss.config.js, utils.ts, and components.json files Update welcome.tsx and package.json files Add tailwind.config.js and update app.css file

--- apps/superligfrfrontend2/app/welcome/welcome.tsx
+++ apps/superligfrfrontend2/app/welcome/welcome.tsx
@@ -1,7 +1,9 @@
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-     test
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </main>
   );
 }
