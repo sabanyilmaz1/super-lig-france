8d3bd0a4e23eefaf643ddd640049e0fb905fc65f
Saban yilmaz <syilmaz002@ensc.fr>
2025-02-03 21:47:51 +0100

    Add @tanstack/react-query dependency

--- apps/superligfrfrontend2/app/root.tsx
+++ apps/superligfrfrontend2/app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -22,6 +23,8 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
+
+const queryClient = new QueryClient();
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -42,7 +45,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return <QueryClientProvider client={queryClient}>
+         <Outlet />
+       </QueryClientProvider>;
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
