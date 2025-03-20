4703b312e0e530fc10223a0aff223f9bbeb71b07
Saban yilmaz <syilmaz002@ensc.fr>
2025-03-20 08:21:28 +0100

    Refactor blog article button to use React Router Link and add new BlogAllArticlesPage component for improved navigation and structure

--- apps/superligfrfrontend2/app/features/blog/components/button-all-articles.tsx
+++ apps/superligfrfrontend2/app/features/blog/components/button-all-articles.tsx
@@ -1,14 +1,15 @@
 import { ArrowRight } from "lucide-react";
+import { Link } from "react-router";
 import { Button } from "~/components/ui/button";
 
 export const ButtonAllArticles = () => {
   return (
     <div className="flex justify-end w-full mt-8">
       <Button variant="link" className="w-full sm:w-auto" asChild>
-        <a href={"https://www.superlig.fr/blog"} target="_blank">
+        <Link to={"/blog/all-articles"}>
           Voir tous les articles
           <ArrowRight className="ml-2 size-4" />
-        </a>
+        </Link>
       </Button>
     </div>
   );
---
+++ apps/superligfrfrontend2/app/routes/private/blog-all-articles.tsx
@@ -0,0 +1,9 @@
+import { HeaderPage } from "~/components/common/header-page";
+
+export default function BlogAllArticlesPage() {
+  return (
+    <section className="min-h-screen">
+      <HeaderPage title="Blog" subtitle="Tous les articles" />
+    </section>
+  );
+}
